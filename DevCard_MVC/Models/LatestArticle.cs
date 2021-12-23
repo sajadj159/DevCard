f2398d01a01f8a3b82b64a15987386c75c4c5353
sajad jafari <sajadj159@hotmail.com>
2021-12-24 03:07:17 +0330

    LatestArticles add to ViewComponent

---
+++ DevCard_MVC/Models/LatestArticle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace DevCard_MVC.Models
+{
+    public class LatestArticle
+    {
+        public long Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+
+        public LatestArticle(long id, string title, string description, string image)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+        }
+
+        public static List<LatestArticle> GetLatestArticles()
+        {
+            return new List<LatestArticle>
+            {
+                new(1, "آموزش Asp.net core mvc", "کاملترین پکیج آموزش Asp.net core mvc به زبان فارسی ",
+                    "blog-post-thumb-card-1.jpg"),
+                new(2, "آموزش Git  & Github", "کاملترین پکیج آموزش Git & Github به زبان فارسی ",
+                    "blog-post-thumb-card-2.jpg"),
+                new(3, "آموزش Onion Architecture", "کاملترین پکیج آموزش Onion Architecture به زبان فارسی ",
+                    "blog-post-thumb-card-3.jpg"),
+                new(4, "آموزش طراحی سایت", "کاملترین پکیج آموزش طراحی سایت به زبان فارسی", 
+                    "blog-post-thumb-card-4.jpg")
+            };
+        }
+    }
+}
---
+++ DevCard_MVC/ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent  :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var latestArticles = LatestArticle.GetLatestArticles();
+            return View("_LatestArticels",latestArticles);
+        }
+    }
+}
