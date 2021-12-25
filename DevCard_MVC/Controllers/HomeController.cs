933327bee481ae7fd68746708150808cf87b41f3
sajad jafari <sajadj159@hotmail.com>
2021-12-25 18:05:15 +0330

    Contact from side validation implemented

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DevCard_MVC.Models;
+﻿using System;
+using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,21 +7,30 @@ namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Contact()
         {
-            return View();
+            var form = new Contact();
+            return View(form);
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Contact form)
+        {
+            
+            Console.WriteLine(form);
+            return Json(Ok());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
     }
 }
---
+++ DevCard_MVC/Models/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevCard_MVC.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "این فیلد اجباریست")]
+        [MinLength(3, ErrorMessage = "حداقل باید بیشتر از 3 کارکتر باشد")]
+        [MaxLength(100, ErrorMessage = "این فیلد نباید بیشتر از 100 کارکتر باشد")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباریست")]
+        [EmailAddress(ErrorMessage = "به درستی وارد نشده است")]
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+        public string Service { get; set; }
+
+        public Contact(string name, string email, string message, string service)
+        {
+            Name = name;
+            Email = email;
+            Message = message;
+            Service = service;
+        }
+
+        public Contact()
+        {
+             
+        }
+    }
+}
