c0cf083f4e568451632f77125fffeabacc9f5aa4
sajad jafari <sajadj159@hotmail.com>
2021-12-26 01:04:56 +0330

    In Cantact Class Deleted private sets

--- DevCard_MVC/Models/Contact.cs
+++ DevCard_MVC/Models/Contact.cs
@@ -8,19 +8,19 @@ namespace DevCard_MVC.Models
         [Required(ErrorMessage = "این فیلد اجباریست")]
         [MinLength(3, ErrorMessage = "حداقل باید بیشتر از 3 کارکتر باشد")]
         [MaxLength(100, ErrorMessage = "این فیلد نباید بیشتر از 100 کارکتر باشد")]
-        public string Name { get; private set; }
+        public string Name { get; set; }
 
         [Required(ErrorMessage = "این فیلد اجباریست")]
         [EmailAddress(ErrorMessage = "ایمیل به درستی وارد نشده است")]
-        public string Email { get; private set; }
+        public string Email { get; set; }
 
-        public string Message { get; private set; }
-        public int Service { get; private set; }
+        public string Message { get; set; }
+        public int Service { get; set; }
 
-        public SelectList Services { get;  set; }
+        public SelectList Services { get; set; }
 
 
-        public Contact(string name, string email, string message, int service,SelectList services)
+        public Contact(string name, string email, string message, int service, SelectList services)
         {
             Name = name;
             Email = email;
@@ -31,7 +31,7 @@ namespace DevCard_MVC.Models
 
         public Contact()
         {
-             
+
         }
     }
 }
